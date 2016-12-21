8b0be0dc50d5e297b2c9d84c6af518ed69cbb69d
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-21 10:25:11 +0200

    Added Deal button click event. Added card randomizing on Deal click.

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0;
+    
+    let cardNames: Array<String> = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: UIButton) {
+        let leftNumber: Int = Int(arc4random_uniform(13))
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        let rightNumber: Int = Int (arc4random_uniform(13))
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        
+    }
 
 }
 
